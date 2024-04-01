ae251123168b9305f5c6c6e48ee8802c427189bc
ASHISHTIME <155893081+BWFTIME@users.noreply.github.com>
2024-04-02 02:24:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/RRomeo-RJ/Romeo-musicBot",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Bwf")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
